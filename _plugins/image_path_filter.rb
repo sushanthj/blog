c035886ba80e691caf833228ea07a72dfe79b473
sushanth <sushanth.jayanth@gmail.com>
2025-05-17 16:32:41 -0700

    again modified image path filter

--- _plugins/image_path_filter.rb
+++ _plugins/image_path_filter.rb
@@ -3,9 +3,18 @@ module Jekyll
     def self.add_baseurl_to_images(content)
       return content unless content.is_a?(String)
       
-      # Handle both ![alt](path) and ![](path) syntax
-      content.gsub(/(!\[(.*?)\]\((\/[^)]*)\))/) do |match|
-        "![#{$2}]({{ site.baseurl }}#{$3})"
+      # More permissive regex that will catch any markdown image syntax
+      # including those with or without alt text, and with or without leading slash
+      content.gsub(/!\[(.*?)\]\((.*?)\)/) do |match|
+        alt_text = $1
+        path = $2
+        
+        # Only add baseurl if the path starts with a slash
+        if path.start_with?('/')
+          "![#{alt_text}]({{ site.baseurl }}{{ page.url | replace: '.html', '' }}/#{path[1..-1]})"
+        else
+          match
+        end
       end
     end
   end
@@ -22,5 +31,7 @@ end
 
 # Add a hook to process markdown files
 Jekyll::Hooks.register [:posts, :pages, :documents], :pre_render do |doc|
-  doc.content = Jekyll::ImagePathFilter.add_baseurl_to_images(doc.content)
+  if doc.respond_to?(:content) && doc.content.is_a?(String)
+    doc.content = Jekyll::ImagePathFilter.add_baseurl_to_images(doc.content)
+  end
 end 
