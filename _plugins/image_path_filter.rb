11faa4ba1c2d5f03a17d18946fa6df73b3c09431
sushanth <sushanth.jayanth@gmail.com>
2025-05-17 16:16:13 -0700

    trying to fix image path filter

--- _plugins/image_path_filter.rb
+++ _plugins/image_path_filter.rb
@@ -3,20 +3,20 @@ module Jekyll
     def self.add_baseurl_to_images(content)
       return content unless content.is_a?(String)
       
-      # Match markdown image syntax
-      content = content.gsub(/(!\[.*?\]\()(\/images\/.*?\))/, "\\1{{ site.baseurl }}\\2")
+      # Match markdown image syntax for any path starting with /
+      content = content.gsub(/(!\[.*?\]\()(\/[^)]*\))/, "\\1{{ site.baseurl }}\\2")
       
-      # Match HTML img tags with src="/images/..."
-      content = content.gsub(/(<img[^>]*src=")(\/images\/[^"]*")/, "\\1{{ site.baseurl }}\\2")
+      # Match HTML img tags with src="/..."
+      content = content.gsub(/(<img[^>]*src=")(\/[^"]*")/, "\\1{{ site.baseurl }}\\2")
       
-      # Match HTML img tags with src='/images/...'
-      content = content.gsub(/(<img[^>]*src=')(\/images\/[^']*')/, "\\1{{ site.baseurl }}\\2")
+      # Match HTML img tags with src='/...'
+      content = content.gsub(/(<img[^>]*src=')(\/[^']*')/, "\\1{{ site.baseurl }}\\2")
       
-      # Match background-image: url('/images/...')
-      content = content.gsub(/(background-image:\s*url\()(\/images\/[^)]*\))/, "\\1{{ site.baseurl }}\\2")
+      # Match background-image: url('/...')
+      content = content.gsub(/(background-image:\s*url\()(\/[^)]*\))/, "\\1{{ site.baseurl }}\\2")
       
-      # Match background-image: url("/images/...")
-      content = content.gsub(/(background-image:\s*url\(")(\/images\/[^"]*"\))/, "\\1{{ site.baseurl }}\\2")
+      # Match background-image: url("/...")
+      content = content.gsub(/(background-image:\s*url\(")(\/[^"]*"\))/, "\\1{{ site.baseurl }}\\2")
       
       content
     end
